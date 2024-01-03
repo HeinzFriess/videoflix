7ef44e222cc49bb642dabfa0c8c51021d35ee791
Heinz Friess <friess.heinz@gmx.de>
2024-01-03 15:02:20 +0100

    first components and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LandingpageComponent } from './content/landingpage/landingpage.component';
+import { MainpageComponent } from './content/mainpage/mainpage.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingpageComponent,
+    MainpageComponent,
+    LoginpageComponent
   ],
   imports: [
     BrowserModule,
